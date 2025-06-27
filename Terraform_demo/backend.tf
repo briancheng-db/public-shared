3851dfb74a599896ca1306de63582a2464075725
briancheng-db <brian.cheng@databricks.com>
2025-06-27 17:07:53 +0800

    fix backend config

--- Terraform_demo/backend.tf
+++ Terraform_demo/backend.tf
@@ -1,4 +1,8 @@
 terraform {
   backend "azurerm" {
+    use_azuread_auth     = true
+    tenant_id = var.azure_sp_tenant_id
+    client_id = var.azure_sp_client_id
+    client_secret = var.azure_sp_client_secret
   }
 }
